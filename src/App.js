0c8264766e76161f83ead4c4f0063b026cb37b86
RoyalOttoc <jong.kim8706@gmail.com>
2020-09-28 05:34:54 +0000

    state added

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-function Food({name, picture, rating}){
-  return(
-    <div>
-      <h1>I like {name}</h1>
-      <h3>{rating} / 5.0</h3>
-      <img src={picture} alt={name}/>
-    </div>
-    
-  )
-}
-
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number.isRequired,
-}
-const data = [{
-  id:1,
-  name: "Bibimbap",
-  image: "https://www.thespruceeats.com/thmb/4Qckb9A9KGyG-q_czTZieMWCGgk=/2048x1367/filters:fill(auto,1)/classic-korean-bibimbap-recipe-2118765_20_preview-5b1ee284ba61770037c8ad38.jpeg",
-  rating: 4 
-},
-{
-  id:2,
-  name: "Kimbap",
-image: "https://www.thespruceeats.com/thmb/bXEyHCT-3algVEAy6GNDXnuCg3Y=/4288x2412/smart/filters:no_upscale()/kimbap-korean-sushi-rolls-2118795-Hero-5b7dbdd346e0fb00250718b8.jpg",
-rating: 4.3 
-},
-{
-  id:3,
-  name: "chapchae",
-  image: "https://i2.wp.com/hipfoodiemom.com/wp-content/uploads/2012/09/img_2169-1.jpg",
-  rating: 5 
-}
-]
-
-function App() {
-  return (
-    <div className="App">
-      {data.map(dish=>(<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>)
-        
-      )}
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count:0
+  };
+  add = () => {
+    console.log("add")
+  };
+  minus = () => {
+    console.log("minus")
+  };
+  render(){
+    return(
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick = {this.add}>add</button>
+        <button onClick = {this.minus}>minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
